7c4de845a41d6d3455d754795ec8615eaadae354
Allan Vargas <alnvar2552@gmail.com>
2025-11-14 10:10:32 -0600

    doing changes as it should

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\allan\Downloads\SnowFlake Hands on\MY TESTS\venv"
+set "VIRTUAL_ENV=C:\Users\allan\Downloads\SnowFlake Hands on\MY TESTS\BTC\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
